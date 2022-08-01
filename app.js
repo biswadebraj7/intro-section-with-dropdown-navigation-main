98e1d868426e8f4bc3c0246f24e71f9b847b16ef
BR-biswadeb-Raj <89997981+BR-biswadeb-Raj@users.noreply.github.com>
2022-08-02 00:59:38 +0600

    I have used html, css, javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const bar=document.querySelector(".fa-bars");
+const navbar=document.querySelector(".navbar");
+bar.addEventListener("click",()=>{
+    navbar.classList.toggle("current");
+});
+console.log("hello")
